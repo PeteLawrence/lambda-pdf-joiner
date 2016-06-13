0590e09a5f5b0717a92d3a88fff0d210ca123549
Pete Lawrence <petejlawrence@icloud.com>
2016-06-13 12:51:48 +0100

    Working version, using request instead of http for downloading, to handle larger downloads without errors

---
+++ src/config.sample.js
@@ -0,0 +1,4 @@
+module.exports = {
+  bucket: 'bucket-name',
+  expiry: 60
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 var fs = require('fs');
-var http = require('https');
+var request = require('request');
+var path = require("path");
+var url = require("url");
+var spawn = require('child_process').spawn;
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+var config = require('./config.js');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 process.env['LD_LIBRARY_PATH'] = process.env['LD_LIBRARY_PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
@@ -7,23 +14,64 @@ process.env['LD_LIBRARY_PATH'] = process.env['LD_LIBRARY_PATH'] + ':' + process.
 
 exports.handler = function(event, context, callback) {
   var promises = [];
+  var filenames = [];
 
   //Download each of the files
-  event.input.forEach(function(inputFile) {
+  event.input.forEach(function(inputUrl) {
+    var fileName = path.basename(url.parse(inputUrl).pathname);
+    filenames.push('/tmp/' + fileName);
+
     promises.push(new Promise(function(resolve, reject) {
-      var fileName = Math.random().toString(36).slice(2) + '.pdf';
-      var file = fs.createWriteStream(fileName);
+      var file = fs.createWriteStream('/tmp/' + fileName);
 
-      var request = http.get(inputFile, function(response) {
-        response.pipe(file);
+      var stream = request.get(inputUrl).pipe(file);
+      stream.on('finish', function() {
+        resolve();
       });
-
-      resolve();
-    }));
+    }.bind()));
   }.bind());
 
+  //Once all files have been downloaded, perform the concatenation
   Promise.all(promises).then(function(values) {
-    console.log('all downloaded');
+    console.log('fooo');
+    var params = filenames;
+    params.push('cat');
+    params.push('output');
+    params.push('/tmp/out.pdf');
+    console.log(params);
+    var child = spawn('pdftk', params);
+
+    child.stdout.on('data', function (data) {
+  	  console.log('stdout: ' + data);
+	  });
+    child.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+  	child.on('close', function (code) {
+  		console.log('child process exited with code ' + code);
+
+      //Generate a filename for the output
+      var filename = Math.random().toString(36).slice(2) + '.pdf';
+
+      //Save the object to S3
+      s3.putObject({
+        Bucket : config.bucket,
+        Key : filename,
+        Body : fs.createReadStream('/tmp/out.pdf'),
+        ContentType : "application/pdf"
+      }, function(error, data) {
+
+        if (error != null) {
+          callback('Unable to upload report');
+        } else {
+          //Generate a signed URL
+          var signedUrl = s3.getSignedUrl('getObject', { Bucket: config.bucket, Key: filename, Expires: config.expiry });
+
+          callback(null, { url: signedUrl });
+        }
+      });
+  	});
+
   });
 
 }
